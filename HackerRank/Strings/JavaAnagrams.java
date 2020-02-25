a91f77799a6b90bd604f80fb64303297fb75ee3f
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-02-25 12:09:58 +0530

    Update JavaAnagrams.java

--- HackerRank/Strings/JavaAnagrams.java
+++ HackerRank/Strings/JavaAnagrams.java
@@ -45,3 +45,11 @@ static boolean isAnagram(String a, String b) {
     }
 
   public static void main(String[] args) {
+           Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
