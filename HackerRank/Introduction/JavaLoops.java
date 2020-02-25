802517825d7cd90a5fda2a402af0a06b5ce9ad8c
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-02-25 11:14:21 +0530

    Update JavaLoops.java

--- HackerRank/Introduction/JavaLoops.java
+++ HackerRank/Introduction/JavaLoops.java
@@ -22,32 +22,23 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class JavaLoops {
-    static void printResults(int a, int b, int n)
-    {
-        int answer = 0;
-        int tempAnswer = 0;
-        for(int i = 0; i<n;i++){
-            tempAnswer = 0;
-            for(int j = 0;j<=i;j++){ 
-                tempAnswer = tempAnswer +(int)(Math.pow(2.0,j)*b);
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int calc = a;
+            for(int j=0;j<n;j++)
+            {
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
             }
-            tempAnswer+=a;
-            System.out.print(tempAnswer + " ");
+            System.out.println();
         }
-        System.out.print("\n");
-        
-    }
-    public static void main(String[] args) {
-       
-        Scanner sc = new Scanner(System.in);
-        int numCases = sc.nextInt();
-        for(int i = 0;i<numCases;i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int n = sc.nextInt();
-            printResults(a,b,n);
-            
-        }
+        in.close();
     }
 }
