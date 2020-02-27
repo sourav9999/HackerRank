90229c63df1c0cf0ecc86d64263d32f69d428487
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-02-27 18:02:43 +0530

    Update JavaIterator.java

--- HackerRank/Java/OOPS/JavaIterator.java
+++ HackerRank/Java/OOPS/JavaIterator.java
@@ -36,44 +36,38 @@ Note: The stdin doesn't contain the string "###", it is added in the main method
 */
 
 import java.util.*;
-public class JavaIterator
-{
-   static Iterator func(ArrayList mylist)
-   {
+public class Main{
+	
+   static Iterator func(ArrayList mylist){
       Iterator it=mylist.iterator();
-      while(it.hasNext())
-      {
+      while(it.hasNext()){
+          // Scripted followed 2 lines
          Object element = it.next();
-         if(element instanceof String) //Hints: use instanceof operator
-            break;
-      }
+         if(element instanceof String)
+			break;
+		}
       return it;
       
    }
-
-   public static void main(String []argh)
-   {
+   @SuppressWarnings({ "unchecked" })
+   public static void main(String []args){
       ArrayList mylist = new ArrayList();
-      Scanner sc=new Scanner(System.in);
-      int n=sc.nextInt();
-      int m=sc.nextInt();
-      for(int i=0;i<n;i++)
-      {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      for(int i = 0;i<n;i++){
          mylist.add(sc.nextInt());
       }
+      
       mylist.add("###");
-      for(int i=0;i<m;i++)
-      {
+      for(int i=0;i<m;i++){
          mylist.add(sc.next());
       }
       
-      
       Iterator it=func(mylist);
-      while(it.hasNext())
-      {
+      while(it.hasNext()){
          Object element = it.next();
          System.out.println((String)element);
       }
-      
    }
 }
