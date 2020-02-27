ca17795d5ee7923ba33816990147b5c8a7eacdf9
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-02-27 17:42:38 +0530

    Update JavaMethodOverriding2.java

--- HackerRank/Java/OOPS/JavaMethodOverriding2.java
+++ HackerRank/Java/OOPS/JavaMethodOverriding2.java
@@ -16,35 +16,28 @@ import java.util.*;
 import java.io.*;
 
 
-class BiCycle
-{
-   String define_me()
-   {
-      return "a vehicel with pedals.";
-   }
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
 }
 
-class MotorCycle extends BiCycle
-{
-   String define_me()
-   {
-      return "a cycle with an engine.";
-   }
-   
-   MotorCycle()
-   {
-      
-      
-      System.out.println("Hello I am a motorcycle, I am "+ define_me());
-      String temp=super.define_me(); //~~Fix me~~
-      System.out.println("My ancestor is a cycle who is "+ temp );
-   }
-   
-}
-class JavaMethodOverriding2{
-   public static void main(String []argh)
-   {
-      MotorCycle M=new MotorCycle();
-   }
-}
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
 
+		String temp= super.define_me(); //Fixed this line
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
