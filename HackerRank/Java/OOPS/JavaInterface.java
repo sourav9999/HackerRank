754d7f7b2bdc1a54e845b0d51bb11a3c97bcd7d5
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-02-27 16:44:47 +0530

    Update JavaInterface.java

--- HackerRank/Java/OOPS/JavaInterface.java
+++ HackerRank/Java/OOPS/JavaInterface.java
@@ -8,17 +8,46 @@ divisorSum function just takes an integer as input and return the sum of all its
 Read the partially completed code in the editor and complete it. You just need to write the MyCalculator class only. Your class shouldn't be public.
 */
 
-class JavaInterface implements AdvancedArithmetic
-{
-    public int divisorSum(int n)
-    {
-        int sum = 0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                sum+=i;
-               
+import java.util.*;
+interface AdvancedArithmetic{
+  int divisor_sum(int n);
+}
+
+//Write your code here
+class MyCalculator implements AdvancedArithmetic{
+    public int divisor_sum(int n){
+        int sum = 1;
+        if(n == 1){
+            return 1;
+        }
+        
+        for(int i = 2; i <= n/2; i++){
+            if(n % i == 0){
+                sum+= i;
             }
         }
-        return sum;      
+        return sum + n;
+    }
+}
+
+class Solution{
+    public static void main(String []args){
+        MyCalculator my_calculator = new MyCalculator();
+        System.out.print("I implemented: ");
+        ImplementedInterfaceNames(my_calculator);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(my_calculator.divisor_sum(n) + "\n");
+      	sc.close();
+    }
+    /*
+     *  ImplementedInterfaceNames method takes an object and prints the name of the interfaces it implemented
+     */
+    static void ImplementedInterfaceNames(Object o){
+        Class[] theInterfaces = o.getClass().getInterfaces();
+        for (int i = 0; i < theInterfaces.length; i++){
+            String interfaceName = theInterfaces[i].getName();
+            System.out.println(interfaceName);
+        }
     }
 }
