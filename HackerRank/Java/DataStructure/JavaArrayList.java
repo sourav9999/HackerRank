92c0184fe6fdcc85eaacaf90a8afc08252ccf2b7
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-03-05 16:09:23 +0530

    Update JavaArrayList.java

--- HackerRank/Java/DataStructure/JavaArrayList.java
+++ HackerRank/Java/DataStructure/JavaArrayList.java
@@ -53,36 +53,29 @@ ERROR!
 
 import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
-
-    public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-        int numLines = Integer.parseInt(sc.nextLine());
-        ArrayList<ArrayList> listArray = new ArrayList<ArrayList>();
-        for(int i = 0;i<numLines;i++){
-            int numOfIntegers = sc.nextInt();
-            ArrayList<Integer> intArrayList =  new ArrayList<Integer>();
-            for(int j=0;j<numOfIntegers;j++){
-                intArrayList.add(new Integer(sc.nextInt()));
+        public static void main(String []args){
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            int d,q,x,y;
+            ArrayList[] set = new ArrayList[n];
+            for(int i=0;i<n;i++){
+                set[i] = new ArrayList();
+                d = in.nextInt();
+                    for(int j=0;j<d;j++){  
+                        set[i].add(in.nextInt());                
+                    }
             }
-            listArray.add(intArrayList);
-            sc.nextLine();
-        }
-        int numQueries = Integer.parseInt(sc.nextLine());
-        for(int i=0;i<numQueries;i++){
-            int x = sc.nextInt()-1;
-            int y = sc.nextInt()-1;
-            sc.nextLine();
-            if(x<listArray.size() && y<listArray.get(x).size()){
-                System.out.println(listArray.get(x).get(y));
-            }else{
+            q=in.nextInt();
+            for(int i=0;i<q;i++){
+                x=in.nextInt();
+                y=in.nextInt();
+                try{
+                    System.out.println(set[x-1].get(y-1));
+                } catch(Exception e){
                     System.out.println("ERROR!");
                 }
+            }
         }
-    }
 }
