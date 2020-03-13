5477b656751c2a95a91c0fe101a3b5b9c0dceaef
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-03-13 11:39:13 +0530

    Update JavaGenerics.java

--- HackerRank/Java/DataStructure/JavaGenerics.java
+++ HackerRank/Java/DataStructure/JavaGenerics.java
@@ -17,13 +17,35 @@ Do not use method overloading because your answer will not be accepted.
 
 */
 
-class Printer
-{
-    public static void printArray(Object[] arr)
-    {
-        for(int i = 0;i<arr.length;i++){
-            System.out.println(""+arr[i]); 
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer {
+    public <T> void printArray(T[] array) {
+        for (T item : array) {
+            System.out.println(item);
         }
-        
+    }
+}
+
+public class Solution {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
     }
 }
