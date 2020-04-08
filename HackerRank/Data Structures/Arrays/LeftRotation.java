43f973e484d9cb726c923f7f3ac57ae6998e168d
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-04-08 15:40:57 +0530

    Update LeftRotation.java

--- HackerRank/Data Structures/Arrays/LeftRotation.java
+++ HackerRank/Data Structures/Arrays/LeftRotation.java
@@ -1,3 +1,16 @@
+/*
+Problem Statement:- https://www.hackerrank.com/challenges/array-left-rotation/problem
+
+
+Sample Input
+5 4  // elemenet, roatation
+1 2 3 4 5
+
+Sample Output
+5 1 2 3 4
+*/
+
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
