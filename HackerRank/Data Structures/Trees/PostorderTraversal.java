1cfa46219bb48a8fff6488253fa6ad37cd45337b
Sourav Panda <36293465+sourav9999@users.noreply.github.com>
2020-04-19 15:54:12 +0530

    Create PostorderTraversal.java

---
+++ HackerRank/Data Structures/Trees/PostorderTraversal.java
@@ -0,0 +1,84 @@
+/*
+Problem Statement:- https://www.hackerrank.com/challenges/tree-postorder-traversal/problem
+
+Sample Input
+     1
+      \
+       2
+        \
+         5
+        /  \
+       3    6
+        \
+         4
+         
+Sample Output
+4 3 6 5 2 1 
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class Solution {
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+}
+
+*/
+
+    public static void postOrder(Node root) {
+        if(root == null){
+            return ;
+        }
+        
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        postOrder(root);
+    }	
+}
